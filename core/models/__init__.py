789a51cd900a8f9e2c90d261ed0d454435e28e31
Дарья Коннова <dashustic@yandex.ru>
2023-12-09 18:55:55 +0300

    create order_product_association

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,7 +1,19 @@
-__all__ = ("Base", "Product", "db_helper", "DataBaseHelper", "User", "Post", "Profile")
+__all__ = (
+    "Base",
+    "Product",
+    "db_helper",
+    "DataBaseHelper",
+    "User",
+    "Post",
+    "Profile",
+    "Order",
+    "order_product_association_table",
+)
 
 from .base import Base
 from .db_helper import DataBaseHelper, db_helper
+from .order import Order
+from .order_product_association import order_product_association_table
 from .post import Post
 from .product import Product
 from .profile import Profile
