95cc051c80a957394950dfc311ee744bc73a78f4
Дарья Коннова <dashustic@yandex.ru>
2023-12-09 18:56:33 +0300

    create order_product_association table

--- alembic/versions/2023_12_09_0310-c3411ee832ca_create_order.py
+++ alembic/versions/2023_12_09_0310-c3411ee832ca_create_order.py
@@ -7,9 +7,9 @@ Create Date: 2023-12-09 03:10:43.241062
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "c3411ee832ca"
