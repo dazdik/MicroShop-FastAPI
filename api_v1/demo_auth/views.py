408238fa4a04cd803c7623013df758d39eb9652e
Дарья Коннова <dashustic@yandex.ru>
2023-12-18 00:08:32 +0300

    token auth

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,7 +1,6 @@
 import secrets
-from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Header
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 router = APIRouter(prefix="/demo_auth", tags=["Demo Auth"])
@@ -19,9 +18,24 @@ async def basic_auth_credentials(credentials: HTTPBasicCredentials = Depends(sec
 
 
 username_to_pass = {"admin": "admin", "dazd": "password"}
+static_auth_token_to_username = {
+    "39a6c9f2eff5aa9ded107e02bc": "admin",
+    "9e30ac5a588057b526aeb038164b31f518894c": "dazd",
+}
 
 
-def get_auth_user(credentials: HTTPBasicCredentials = Depends(security)):
+def get_username_by_static_auth_token(
+    static_token: str = Header(alias="x-auth-token"),
+) -> str:
+    if username := static_auth_token_to_username.get(static_token):
+        return username
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
+
+
+def get_auth_user(credentials: HTTPBasicCredentials = Depends(security)) -> str:
     unauth_exc = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid username or password",
@@ -39,4 +53,15 @@ def get_auth_user(credentials: HTTPBasicCredentials = Depends(security)):
 
 @router.get("/basic-auth-username/")
 async def basic_auth_credentials(username: str = Depends(get_auth_user)):
+    return {"message": f"Hi, {username}", "username": username} @ router.get(
+        "/basic-auth-username/"
+    )
+
+
+@router.get("/some_http_headers_auth/")
+async def some_http_headers_auth(
+    username: str = Depends(get_username_by_static_auth_token),
+):
     return {"message": f"Hi, {username}", "username": username}
+
+
