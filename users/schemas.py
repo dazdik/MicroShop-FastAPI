c9a76cb6ab75e87f0421db9b787109f85734b49b
Дарья Коннова <dashustic@yandex.ru>
2024-01-12 12:24:17 +0300

    jwt auth

--- users/schemas.py
+++ users/schemas.py
@@ -1,9 +1,17 @@
 from typing import Annotated
 
 from annotated_types import MaxLen, MinLen
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class CreateUser(BaseModel):
     username: Annotated[str, MinLen(3), MaxLen(20)]
     email: EmailStr
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+    username: str
+    password: bytes
+    email: str | None = None
+    active: bool = True
